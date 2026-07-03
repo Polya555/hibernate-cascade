618a2f215b8d360efe6d91e0f52ba20ac708c8f2
пк <klimashevscapo@gmail.com>
2026-07-04 00:20:52 +0300

    did all required tasks

--- src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
+++ src/main/java/core/basesyntax/dao/impl/UserDaoImpl.java
@@ -25,7 +25,7 @@ public class UserDaoImpl extends AbstractDao implements UserDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't create " + entity, e);
+            throw new RuntimeException("Can't create entity: " + entity, e);
         } finally {
             if (session != null) {
                 session.close();
